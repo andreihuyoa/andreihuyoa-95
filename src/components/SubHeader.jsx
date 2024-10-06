7d26a8d96151a41bcbcbfb10838a48a8ab243f31
Andrei Huyo-a <91776408+andreihuyoa@users.noreply.github.com>
2024-10-06 14:06:26 +0800

    Update SubHeader.jsx
    button nows mapped, can do multiple buttons

--- src/components/SubHeader.jsx
+++ src/components/SubHeader.jsx
@@ -2,14 +2,16 @@ import React from "react";
 
 import Button from "./Button";
 
-const SubHeader = ({ className = "", icon, title, onSend }) => {
+const SubHeader = ({ className = "", buttons = [] }) => {
   return (
     <div
       className={`p-1 border border-95-white outline outline-1 outline-95-darkgray text-sm font-bold text-black ${className}`}
     >
-      <Button onClick={onSend}>
-        <img src={icon} alt={title} className="w-3 h-3" /> {title}
-      </Button>
+      {buttons.map(({ icon, title, onClick }, index) => (
+        <Button key={index} onClick={onClick}>
+          {icon && <img src={icon} title={title} className="w-3 h-3 bg-transparent" />}
+        </Button>
+      ))}
     </div>
   );
 };
