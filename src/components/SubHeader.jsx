440b6e5d1921a220bacf12ec4d60d04a7d09dd94
Andrei Huyo-a <91776408+andreihuyoa@users.noreply.github.com>
2024-12-21 03:46:57 +0800

    tailwind prettier changes

--- src/components/SubHeader.jsx
+++ src/components/SubHeader.jsx
@@ -1,16 +1,16 @@
-import React from "react";
-
 import Button from "./Button";
 
 const SubHeader = ({ className = "", buttons = [] }) => {
   return (
     <div
-      className={`flex gap-1 py-0.5 px-1 border border-95-white outline outline-1 outline-95-darkgray text-sm font-bold text-black ${className}`}
+      className={`flex gap-1 border border-95-white px-1 py-0.5 text-sm font-bold text-black outline outline-1 outline-95-darkgray ${className}`}
     >
       {buttons.map(({ icon, title, onClick }, index) => (
         <Button key={index} onClick={onClick}>
           {/* icon and title */}
-          {icon && <img src={icon} title={title} className="w-3 h-3 bg-transparent" />}
+          {icon && (
+            <img src={icon} title={title} className="h-4 w-4 bg-transparent" />
+          )}
           {title}
         </Button>
       ))}
--- src/components/TaskBar.jsx
+++ src/components/TaskBar.jsx
@@ -1,6 +1,6 @@
 const TaskBar = ({ children }) => {
   return (
-    <div className="flex h-12 items-center border-t-2 border-95-white bg-95-gray px-1">
+    <div className="flex h-12 items-center overflow-hidden border-t-2 border-95-white bg-95-gray px-1">
       {children}
     </div>
   );
