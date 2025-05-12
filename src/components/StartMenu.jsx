f28108b8aed104409dffa08945706b27fccfb046
Andrei Huyo-a <91776408+andreihuyoa@users.noreply.github.com>
2025-05-12 23:54:24 +0800

    fixes

--- src/components/StartMenu.jsx
+++ src/components/StartMenu.jsx
@@ -15,10 +15,10 @@ const StartMenu = ({ isOpen, onClose }) => {
     return null;
   }
   return (
-    <div className="flex text-sm font-MS95 absolute bottom-12 left-0 overflow-hidden z-[9999] bg-95-gray border-2 border-95-white border-r-95-black border-b-95-black ">
+    <div className="absolute bottom-12 left-0 z-[9999] flex overflow-hidden border-2 border-95-white border-b-95-black border-r-95-black bg-95-gray font-MS95 text-sm">
       {/* Sidebar */}
       <div className="flex items-stretch">
-        <img src={sidebarImage} alt="AndreiHuyoa95" className="w-6 h-auto" />
+        <img src={sidebarImage} alt="AndreiHuyoa95" className="h-auto w-6" />
       </div>
       <div className="flex-grow justify-between">
         {/* Socials */}
@@ -32,7 +32,11 @@ const StartMenu = ({ isOpen, onClose }) => {
           link="https://www.linkedin.com/in/carl-andrei-del-rosario-3bab57257/"
           text="LinkedIn"
         />
-        <SocialsButton icon={git} link="https://www.github.com/andreihuyoa" text="Github" />
+        <SocialsButton
+          icon={git}
+          link="https://www.github.com/andreihuyoa"
+          text="Github"
+        />
         <div className="my-2 border-t border-gray-500"></div>
         <SocialsButton icon={file} link={HisResume} text="Resume" />
         <SocialsButton icon={project1} link="" text="Lokalista" />
